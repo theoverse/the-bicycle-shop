f2b08500c711ed86722a65dce53b622a8903d311
theoverse <theoverse@protonmail.com>
2022-02-26 14:50:40 -0600

    declared routes and renderde html

--- index.js
+++ index.js
@@ -1,21 +1,32 @@
 
 const http = require('http')
 const url = require('url')
+const fs = require('fs').promises
 
-const server = http.createServer((req, res) => {
-    // console.log('Server is now running')
+const server = http.createServer(async (req, res) => {
 
-    // console.log(req.headers)
     if (req.url === '/favicon.ico') return
-    // console.log(req.url)
+
     const myUrl = new URL(req.url, `http://${req.headers.host}`)
-    // console.log(myUrl)
     const pathname = myUrl.pathname
     const id = myUrl.searchParams.get('id')
-    // console.log(pathname, id)
 
-    res.writeHead(200, { 'Content-Type': 'text/html' })
-    res.end('<h1>Welcome to the bicycle shop</h1>')
+    if (pathname === '/') {
+
+        const html = await fs.readFile('./views/bicycles.html', 'utf-8')
+        res.writeHead(200, { 'Content-Type': 'text/html' })
+        res.end(html)
+
+    } else if (pathname === '/bicycle' && id >= 0 && id <= 5) {
+
+        const html = await fs.readFile('./views/overview.html', 'utf-8')
+        res.writeHead(200, { 'Content-Type': 'text/html' })
+        res.end(html)
+
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/html' })
+        res.end('<div><h1>File Not Found</h1></div>')
+    }
 })
 
 server.listen(3000)
