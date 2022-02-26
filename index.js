66720139964ccb274459d4bb0830b310c1a3a842
theoverse <theoverse@protonmail.com>
2022-02-26 11:42:54 -0600

    init npm, installed nodemon, and created http server

---
+++ index.js
@@ -0,0 +1,10 @@
+
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+    // console.log('Server is now running')
+    res.writeHead(200, { 'Content-Type': 'text/html' })
+    res.end('<h1>Welcome to the bicycle shop</h1>')
+})
+
+server.listen(3000)
