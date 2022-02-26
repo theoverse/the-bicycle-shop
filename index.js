07f526deba35ad46b6265d12d5bec6971496f47a
theoverse <theoverse@protonmail.com>
2022-02-26 17:26:48 -0600

    added dynamic data rendering on the overview page

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 const http = require('http')
 const url = require('url')
 const fs = require('fs').promises
+const bicycles = require('./data/data.json')
+// console.log(bicycles)
+
 
 const server = http.createServer(async (req, res) => {
 
@@ -10,8 +13,6 @@ const server = http.createServer(async (req, res) => {
     const myUrl = new URL(req.url, `http://${req.headers.host}`)
     const pathname = myUrl.pathname
     const id = myUrl.searchParams.get('id')
-
-    // console.log(req.url)
 
     if (pathname === '/') {
 
@@ -23,10 +24,21 @@ const server = http.createServer(async (req, res) => {
 
     else if (pathname === '/bicycle' && id >= 0 && id <= 5) {
 
-        const html = await fs.readFile('./views/overview.html', 'utf-8')
+        let html = await fs.readFile('./views/overview.html', 'utf-8')
+
+        const bicycle = bicycles.find(b => b.id === id)
+        // console.log(bicycle)
+        html = html.replace(/<%IMAGE%>/g, bicycle.image)
+        html = html.replace(/<%NAME%>/g, bicycle.name)
+
+        let price = bicycle.originalPrice
+        if (bicycle.hasDiscount) {
+            price = (price * (100 - bicycle.discount)) / 100
+        }
+        html = html.replace(/<%NEWPRICE%>/g, `$${price}`)
+
         res.writeHead(200, { 'Content-Type': 'text/html' })
         res.end(html)
-
     }
 
     else if (/\.(png)$/i.test(req.url)) {
