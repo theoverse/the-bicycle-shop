a0fdc21d990ff8936bcd807f94fd620f74a91176
theoverse <theoverse@protonmail.com>
2022-02-26 19:05:37 -0600

    added rendering dynamic data for homepage

--- index.js
+++ index.js
@@ -22,9 +22,9 @@ const server = http.createServer(async (req, res) => {
 
         let allTheBicycles = ''
         for (let index = 0; index < 6; index++) {
-            allTheBicycles += AllMainBicycles
+            allTheBicycles += replaceTemplate(AllMainBicycles, bicycles[index])
         }
-        html = html.replace(/<%AllMainBicycles%>/g, `$${allTheBicycles}`)
+        html = html.replace(/<%AllMainBicycles%>/g, `${allTheBicycles}`)
 
         res.writeHead(200, { 'Content-Type': 'text/html' })
         res.end(html)
@@ -36,15 +36,8 @@ const server = http.createServer(async (req, res) => {
         let html = await fs.readFile('./views/overview.html', 'utf-8')
 
         const bicycle = bicycles.find(b => b.id === id)
-        // console.log(bicycle)
-        html = html.replace(/<%IMAGE%>/g, bicycle.image)
-        html = html.replace(/<%NAME%>/g, bicycle.name)
 
-        let price = bicycle.originalPrice
-        if (bicycle.hasDiscount) {
-            price = (price * (100 - bicycle.discount)) / 100
-        }
-        html = html.replace(/<%NEWPRICE%>/g, `$${price}`)
+        html = replaceTemplate(html, bicycle)
 
         res.writeHead(200, { 'Content-Type': 'text/html' })
         res.end(html)
@@ -75,3 +68,30 @@ const server = http.createServer(async (req, res) => {
 })
 
 server.listen(3000)
+
+function replaceTemplate(html, bicycle) {
+    html = html.replace(/<%IMAGE%>/g, bicycle.image)
+    html = html.replace(/<%NAME%>/g, bicycle.name)
+
+    let price = bicycle.originalPrice
+    if (bicycle.hasDiscount) {
+        price = (price * (100 - bicycle.discount)) / 100
+    }
+    html = html.replace(/<%NEWPRICE%>/g, `$${price}.00`)
+    html = html.replace(/<%OLDPRICE%>/g, `$${bicycle.originalPrice}`)
+    html = html.replace(/<%ID%>/g, bicycle.id)
+
+    if (bicycle.hasDiscount) {
+        html = html.replace(/<%DISCOUNTRATE%>/g,
+            `<div class="discount__rate"><p>${bicycle.discount}%</p></div>`)
+    } else {
+        html = html.replace(/<%DISCOUNTRATE%>/g, ` `)
+    }
+
+    for (let index = 0; index < bicycle.star; index++) {
+        html = html.replace(/<%STAR%>/, 'checked')
+    }
+    html = html.replace(/<%STAR%>/g, '')
+
+    return html
+}
